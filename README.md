2491080427ddeedd1c337595a72950d92a8e1e40
James Bourne <james.bourne@infinityworks.com>
2019-05-31 15:54:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# AWSv4 Axios
+
+This is a request interceptor for the Axios HTTP request library to allow requests to be signed with an AWSv4 signature.
+
+This may be useful for accessing AWS services protected with IAM auth such as an API Gateway.
---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import { sign } from "aws4";
+import { AxiosRequestConfig } from "axios";
+
+interface InterceptorOptions {
+  service?: string;
+  region?: string;
+}
+
+interface SigningOptions {
+  host?: string;
+  headers?: {};
+  path?: string;
+  body?: any;
+  region?: string;
+  service?: string;
+}
+
+export const interceptor = (options?: InterceptorOptions) => (
+  config: AxiosRequestConfig
+) => {
+  if (!config.url) {
+    throw new Error("No URL present in request config, unable to sign request");
+  }
+
+  const { host, pathname } = new URL(config.url);
+
+  let region: string | undefined;
+  let service: string | undefined;
+
+  if (options) {
+    ({ region, service } = options);
+  }
+
+  const signingOptions: SigningOptions = {
+    host,
+    path: pathname,
+    headers: {},
+    region,
+    service
+  };
+
+  sign(signingOptions);
+
+  config.headers = signingOptions.headers;
+
+  return config;
+};

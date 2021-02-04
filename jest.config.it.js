2e1fd3444cbce4ee58557ec3d5a7c2d111d53b38
Maciej Radzikowski <maciej.radzikowski@merapar.com>
2021-02-04 16:38:14 +0100

    test: Serverless stack with API for integration tests

--- jest.config.it.js
+++ jest.config.it.js
@@ -1,4 +1,5 @@
 module.exports = {
   ...require("./jest.config"),
-  testMatch: ["**/__tests__/**/*.[jt]s?(x)"],
+  testMatch: ["**/__tests__/**/*.it.[jt]s?(x)"],
+  globalSetup: "./src/__tests__/setup.ts"
 };
---
+++ src/__tests__/setup.ts
@@ -0,0 +1,35 @@
+import {
+  CloudFormationClient,
+  DescribeStacksCommand,
+} from "@aws-sdk/client-cloudformation";
+
+const region = process.env.AWS_REGION || "eu-west-2";
+const stage = process.env.STAGE || "dev";
+
+module.exports = async () => {
+  const stackName = `aws4AxiosIT-${stage}`;
+
+  const cf = new CloudFormationClient({ region });
+  const stacks = await cf.send(
+    new DescribeStacksCommand({
+      StackName: stackName,
+    })
+  );
+  const stack = stacks.Stacks?.[0];
+
+  if (stack === undefined) {
+    throw new Error(
+      `Couldn't find CloudFormation stack with name ${stackName}`
+    );
+  }
+
+  process.env.API_GATEWAY_URL = stack.Outputs?.find(
+    (o) => o.OutputKey === "HttpApiUrl"
+  )?.OutputValue;
+  process.env.CLIENT_ROLE_ARN = stack.Outputs?.find(
+    (o) => o.OutputKey === "ClientRoleArn"
+  )?.OutputValue;
+
+  process.env.AWS_REGION = region;
+  process.env.STAGE = stage;
+};
